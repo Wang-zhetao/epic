f62689ad62f63a26c13557629ae248276723f5c1
Wzhetao <wzt15857662906@163.com>
2021-05-24 23:57:45 +0800

    搭建初步样式

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import History from "./pages/History";
-import About from "./pages/About";
-
+import Loading from "./components/Loading";
 import { Switch, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/Home"));
+const History = lazy(() => import("./pages/History"));
+const About = lazy(() => import("./pages/About"));
 
 function App() {
   return (
-    <div className="app">
+    // 即使是空的标签也要包裹
+    <>
       <Header />
-
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/history" component={History} />
-        <Route path="/about" component={About} />
-      </Switch>
-
+      <main>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/history" component={History} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Suspense>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
